78f5225d9be618909d1ecd5a0985730dc97bca20
Frédéric Hannes <freddy1990@gmail.com>
2015-05-19 12:29:07 +0200

    Added CommModel

--- src/main/java/be/kuleuven/cs/mas/Main.java
+++ src/main/java/be/kuleuven/cs/mas/Main.java
@@ -6,6 +6,7 @@ import be.kuleuven.cs.mas.parcel.ParcelFactory;
 import be.kuleuven.cs.mas.strategy.FieldStrategy;
 import be.kuleuven.cs.mas.strategy.FieldTimeStrategy;
 import com.github.rinde.rinsim.core.Simulator;
+import com.github.rinde.rinsim.core.model.comm.CommModel;
 import com.github.rinde.rinsim.core.model.pdp.*;
 import com.github.rinde.rinsim.core.model.road.CollisionGraphRoadModel;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
@@ -27,11 +28,13 @@ public class Main {
     private final FieldStrategy parcelFieldStrategy = new FieldTimeStrategy(1000);
     private final ParcelFactory parcelFactory;
 
+    private final CommModel commModel;
     private final RoadModel roadModel;
 
     private final Simulator sim;
 
     public Main() {
+        commModel = CommModel.builder().build();
         roadModel = CollisionGraphRoadModel.builder(GraphUtils.createGraph())
                 .setVehicleLength(GraphUtils.VEHICLE_LENGTH)
                 .build();
@@ -42,6 +45,7 @@ public class Main {
                 GraphUtils.getDropOffSites());
 
         sim = Simulator.builder()
+                .addModel(commModel)
                 .addModel(DefaultPDPModel.create())
                 .addModel(roadModel)
                 .addModel(new GradientModel())
