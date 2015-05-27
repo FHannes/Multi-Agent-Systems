e7977d1039df92b0fe5959775760b3bc83080476
Frédéric Hannes <freddy1990@gmail.com>
2015-05-27 12:01:33 +0200

    Reverted changes

--- src/main/java/be/kuleuven/cs/mas/agent/AgentFactory.java
+++ src/main/java/be/kuleuven/cs/mas/agent/AgentFactory.java
@@ -1,11 +1,6 @@
 package be.kuleuven.cs.mas.agent;
 
 import be.kuleuven.cs.mas.strategy.FieldStrategy;
-import com.github.rinde.rinsim.core.Simulator;
-import com.github.rinde.rinsim.core.model.rand.RandomModel;
-import com.github.rinde.rinsim.core.model.rand.RandomProvider;
-import com.github.rinde.rinsim.core.model.rand.RandomUser;
-import com.github.rinde.rinsim.core.model.road.CollisionGraphRoadModel;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
 import com.github.rinde.rinsim.geom.Point;
 import com.google.common.base.Optional;
@@ -15,28 +10,27 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class AgentFactory implements RandomUser {
+public class AgentFactory {
 
     public final static double CAPACITY = 1.0D;
 
-    private RandomGenerator rng;
+    private final RandomGenerator rng;
     private final FieldStrategy fieldStrategy;
+    private final RoadModel roadModel;
     private final int visualRange;
     private final List<Point> spawnSites;
     private int idCounter = 0;
 
-    public AgentFactory(FieldStrategy fieldStrategy, int visualRange, List<Point> spawnSites) {
+    public AgentFactory(RandomGenerator rng, FieldStrategy fieldStrategy, RoadModel roadModel, int visualRange,
+                        List<Point> spawnSites) {
+        this.rng = rng;
         this.fieldStrategy = fieldStrategy;
+        this.roadModel = roadModel;
         this.visualRange = visualRange;
         this.spawnSites = spawnSites;
     }
 
-    public AGVAgent makeAgent(Simulator sim) {
-        RandomModel rm = sim.getModelProvider().getModel(RandomModel.class);
-        rm.register(this);
-
-        RoadModel roadModel = sim.getModelProvider().getModel(CollisionGraphRoadModel.class);
-
+    public AGVAgent makeAgent() {
         Set<Point> occupiedPoints = roadModel.getObjectsOfType(AGVAgent.class).stream().map(AGVAgent::getPosition)
                 .filter(Optional::isPresent).map(Optional::get).collect(Collectors.toSet());
 
@@ -48,11 +42,6 @@ public class AgentFactory implements RandomUser {
             return new AGVAgent(rng, fieldStrategy, visualRange, freeSpawns.get(rng.nextInt(freeSpawns.size())),
                     String.format("agent%d", ++idCounter), CAPACITY);
         }
-    }
-
-    @Override
-    public void setRandomGenerator(RandomProvider provider) {
-        rng = provider.sharedInstance(AgentFactory.class);
     }
 
 }
--- src/main/java/be/kuleuven/cs/mas/parcel/ParcelFactory.java
+++ src/main/java/be/kuleuven/cs/mas/parcel/ParcelFactory.java
@@ -1,10 +1,6 @@
 package be.kuleuven.cs.mas.parcel;
 
 import be.kuleuven.cs.mas.strategy.FieldStrategy;
-import com.github.rinde.rinsim.core.Simulator;
-import com.github.rinde.rinsim.core.model.rand.RandomModel;
-import com.github.rinde.rinsim.core.model.rand.RandomProvider;
-import com.github.rinde.rinsim.core.model.rand.RandomUser;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
 import com.github.rinde.rinsim.geom.Point;
 import org.apache.commons.math3.random.RandomGenerator;
@@ -14,17 +10,18 @@ import java.util.*;
 /**
  * A factory which can be sued to create instances of {@link TimeAwareParcel}.
  */
-public class ParcelFactory implements RandomUser {
+public class ParcelFactory {
 
     public final static double MAGNITUDE = 1.0D;
 
-    private RandomGenerator rng;
+    private final RandomGenerator rng;
     private final FieldStrategy fieldStrategy;
     private final List<Point> storageSites;
     private final List<Point> ioSites;
 
-    public ParcelFactory(FieldStrategy fieldStrategy, List<Point> storageSites,
+    public ParcelFactory(RandomGenerator rng, FieldStrategy fieldStrategy, List<Point> storageSites,
                          List<Point> ioSites) {
+        this.rng = rng;
         this.fieldStrategy = fieldStrategy;
         this.storageSites = new ArrayList<>(storageSites);
         this.ioSites = new ArrayList<>(ioSites);
@@ -33,14 +30,13 @@ public class ParcelFactory implements RandomUser {
     /**
      * Creates a new {@link TimeAwareParcel} to use in a {@link com.github.rinde.rinsim.core.model.pdp.PDPModel}.
      *
-     * @param sim
      * @param outgoing
      *        The parcel generated has to be outgoing. This means that it is to be picked up from one of the shelves in
      *        the warehouse and transported to a drop-off site where the warehouse's I/O occurs.
      * @param currentTime
      * @return A new {@link TimeAwareParcel} instance.
      */
-    private TimeAwareParcel makeParcel(Simulator sim, boolean outgoing, long currentTime) {
+    public TimeAwareParcel makeParcel(boolean outgoing, long currentTime) {
         // Get set of shelf sites and drop-off sites
         List<Point> sources;
         List<Point> targets;
@@ -65,21 +61,13 @@ public class ParcelFactory implements RandomUser {
         return new TimeAwareParcel(fieldStrategy, source, target, MAGNITUDE, currentTime);
     }
 
-    public TimeAwareParcel makeParcel(Simulator sim, long currentTime) {
-        RandomModel rm = sim.getModelProvider().getModel(RandomModel.class);
-        rm.register(this);
-
+    public TimeAwareParcel makeParcel(long currentTime) {
         boolean outgoing = rng.nextBoolean();
-        TimeAwareParcel parcel = makeParcel(sim, outgoing, currentTime);
+        TimeAwareParcel parcel = makeParcel(outgoing, currentTime);
         if (parcel == null) {
-            parcel = makeParcel(sim, !outgoing, currentTime);
+            parcel = makeParcel(!outgoing, currentTime);
         }
         return parcel;
-    }
-
-    @Override
-    public void setRandomGenerator(RandomProvider provider) {
-        rng = provider.sharedInstance(ParcelFactory.class);
     }
 
 }
